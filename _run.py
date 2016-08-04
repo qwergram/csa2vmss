3c3ebe841e32862bb46e4416056d737709cad0ca
Norton Pengra <npengra317@gmail.com>
2016-08-04 15:48:41 -0700

    linting

--- _run.py
+++ _run.py
@@ -6,21 +6,25 @@ import pyscripts.csa_parse
 
 CURRENT_PATH = os.getcwd()
 
+
 def parse(enum, content):
     if content.startswith("-") and '=' in content:
         return (content.split('=')[0][1:], content.split('=')[1])
     else:
         return enum, content
 
+
 def load_solution(params):
     parsed = pyscripts.csa_parse.VSCloudService(params['Location'])
     parsed.load_solution()
     return parsed.solution_data
 
+
 def run_powershell(name, arguments):
     arguments = " ".join(["-%s \"%s\"" % (key, value) for key, value in arguments.items()])
     execute_this = "powershell -ExecutionPolicy Unrestricted -File \"%s\" %s" % (os.path.join(CURRENT_PATH, 'psscripts', name), arguments)
     os.system(execute_this)
+
 
 def package_projects(solution):
     for project in solution['projects']:
@@ -29,9 +33,11 @@ def package_projects(solution):
             f.write(json.dumps(project, indent=2))
             run_powershell("save_roles.ps1", {"zipfilename": os.path.join(CURRENT_PATH, '__save', project['guid'], "package.zip"), "sourcedir": project['folder'] + "\\" if project['folder'].endswith("\\") else project['folder']})
 
+
 def clean():
     os.system('rm -r %s' % os.path.join(CURRENT_PATH, '__save'))
     os.mkdir(os.path.join(CURRENT_PATH, '__save'))
+
 
 def main():
     clean()
