d9adf980a8bc3fb53f40540c564ed5b24ae43920
Norton Pengra <npengra317@gmail.com>
2016-08-10 08:54:33 -0700

    Update script to include IIS installation

--- psscripts/enable_rmps.ps1
+++ psscripts/enable_rmps.ps1
@@ -1,4 +1,29 @@
-﻿# Ensure PS remoting is enabled, although this is enabled by default for Azure VMs
+﻿# http://stackoverflow.com/questions/10522240/powershell-script-to-auto-install-of-iis-7-and-above
+# --------------------------------------------------------------------
+# Loading Feature Installation Modules
+# --------------------------------------------------------------------
+Import-Module ServerManager 
+
+# --------------------------------------------------------------------
+# Installing IIS
+# --------------------------------------------------------------------
+Add-WindowsFeature -Name Web-Common-Http,Web-Asp-Net,Web-Net-Ext,Web-ISAPI-Ext,Web-ISAPI-Filter,Web-Http-Logging,Web-Request-Monitor,Web-Basic-Auth,Web-Windows-Auth,Web-Filtering,Web-Performance,Web-Mgmt-Console,Web-Mgmt-Compat,RSAT-Web-Server,WAS -IncludeAllSubFeature
+
+# --------------------------------------------------------------------
+# Loading IIS Modules
+# --------------------------------------------------------------------
+Import-Module WebAdministration
+
+
+# --------------------------------------------------------------------
+# Resetting IIS
+# --------------------------------------------------------------------
+$Command = "IISRESET"
+Invoke-Expression -Command $Command
+
+
+# https://blogs.technet.microsoft.com/uktechnet/2016/02/12/create-a-custom-script-extension-for-an-azure-resource-manager-vm-using-powershell/
+# Ensure PS remoting is enabled, although this is enabled by default for Azure VMs
 Enable-PSRemoting -Force
 
 # Create rule in Windows Firewall
