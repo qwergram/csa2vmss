e59156624222e5e057b061e6be271a6bbbff2c79
Norton Pengra <npengra317@gmail.com>
2016-08-12 12:21:53 -0700

    Remove useless files

--- psscripts/enable_web_deploy.ps1
+++
@@ -1,7 +0,0 @@
-# Enable Web Deploy on remote server
-# http://www.iis.net/learn/publish/using-web-deploy/use-the-web-deployment-tool
-
-Invoke-WebRequest -Uri http://go.microsoft.com/fwlink/?LinkID=309497 -OutFile installer.msi
-msiexec /i ./installer.msi /quiet ADDLOCAL=ALL
-
-cmd.exe /c "net start msdepsvc"
