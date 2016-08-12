fbf0e5fe41d552e28be7b91325cbab430e28f87d
Norton Pengra <npengra317@gmail.com>
2016-08-12 14:14:33 -0700

    Specify script to be generated in psscripts dir

--- pyscripts/generate_zip_downloader.py
+++ pyscripts/generate_zip_downloader.py
@@ -1,5 +1,6 @@
 import sys
 import io
+import os
 
 script = """
 Write-Host "Downloading Zip file"
@@ -9,5 +10,5 @@ Invoke-WebRequest -Uri {} -OutFile contents.zip
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    with io.open("autogen.savezip.ps1", 'w') as handle:
+    with io.open(os.path.join("psscripts", "autogen.savezip.ps1"), 'w') as handle:
         handle.write(script.format(url))
