4600e4d64c147dc14b2e87c868226cea9721206b
Norton Pengra <npengra317@gmail.com>
2016-08-16 19:26:16 -0700

    Remove dead Code

--- pyscripts/build_bootstraper.py
+++ pyscripts/build_bootstraper.py
@@ -4,26 +4,6 @@ import json
 import os
 import shutil
 
-def load_appconfig(location):
-    print("Load app.config")
-    with io.open(location) as context:
-        return context.read()
-
-def load_assemblyinfo(location):
-    with io.open(location) as context:
-        return context.read()
-
-def load_csproj(location):
-    with io.open(location) as context:
-        return context.read()
-
-
-def load_bootstrapper(location, workername, classname):
-    with io.open(location) as context:
-        bootstrap = context.read()
-    bootstrap = bootstrap.replace("{$WORKERNAME!}", workername).replace("{$CLASSNAME!}", classname)
-    return bootstrap
-
 def reset_sln(sln_location, to_remove):
     with io.open(sln_location) as context:
         sln_data = context.readlines()
@@ -71,13 +51,5 @@ def main(worker, solution, current_path, zip_package_name):
     shutil.copy(solution['sln'], project_dest)
     reset_sln(packaged_worker_sln, to_remove)
     os.system("C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\MSBuild.exe \"%s\"" % packaged_worker_sln)
-
-
-    # global APPCONFIG, ASSEMBLIES, CSPROJ, BOOTSTRAP
-    # print("Building bootstrapper")
-    # APPCONFIG = load_appconfig()
-    # ASSEMBLIES = load_assemblyinfo()
-    # CSPROJ = load_csproj()
-    # BOOTSTRAP = load_bootstrapper()
 
     return solution
