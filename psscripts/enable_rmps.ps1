56b1670b85ff480362f10f759d8f44b18d74eb5e
Norton Pengra <npengra317@gmail.com>
2016-08-17 08:10:32 -0700

    Download worker role package on remote

--- psscripts/enable_rmps.ps1
+++ psscripts/enable_rmps.ps1
@@ -1,5 +1,11 @@
 # https://blogs.technet.microsoft.com/uktechnet/2016/02/12/create-a-custom-script-extension-for-an-azure-resource-manager-vm-using-powershell/
 # Ensure PS remoting is enabled, although this is enabled by default for Azure VMs
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $urlcontainer
+)
+
 Enable-PSRemoting -Force
 
 # Create rule in Windows Firewall
@@ -15,3 +21,8 @@ $thumbprint = (New-SelfSignedCertificate -DnsName $env:COMPUTERNAME -CertStoreLo
 # Run WinRM configuration on command line. DNS name set to computer hostname.
 $cmd = "winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname=""$env:computername""; CertificateThumbprint=""$thumbprint""}"
 cmd.exe /C $cmd
+
+
+# Download the package for this worker role
+$zipName = ("zip_" + $env:COMPUTERNAME.Substring(0, 4).ToUpper() + "_package.zip")
+Invoke-WebRequest -Uri ($urlcontainer + $zipName) -OutFile project.zip
