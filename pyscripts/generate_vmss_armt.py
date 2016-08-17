60f6f3a5c605a23e3c45135310becc798e262529
Norton Pengra <npengra317@gmail.com>
2016-08-17 16:40:35 -0700

    Look in new json file to load uri

--- pyscripts/generate_vmss_armt.py
+++ pyscripts/generate_vmss_armt.py
@@ -64,9 +64,13 @@ def save_arm_params():
         context.write(json.dumps(VMSS_PARAMS_ARMT, indent=2, sort_keys=True))
     shutil.copy(os.path.join(CURRENT_PATH, "templates", "vmss.json"), os.path.join(location, "vmss.json"))
 
+def load_uri():
+    with io.open(os.path.join(CURRENT_PATH, "__save", "vhd.json")) as context:
+        return json.loads(context.read())['resources'][0]['properties']['storageProfile']['osDisk']['image']['uri']
+
 if __name__ == "__main__":
     NAME = sys.argv[1]
-    URI = sys.argv[2]
+    URI = load_uri()
     SOLUTION_DATA = load_solution_data()
     load_arm_params()
     VMSS_PARAMS_ARMT["parameters"] = PARAMETERS
