26aae3b5bb4cdd3b3dee955d2b44f8fe8371e348
Norton Pengra <npengra317@gmail.com>
2016-08-18 20:21:01 -0700

    Get user to select worker, web and commons

--- pyscripts/pre_script.py
+++ pyscripts/pre_script.py
@@ -11,19 +11,39 @@ def main(solution_path):
         if os.path.isdir(path) and (directory.lower() not in ['backup', '.vs', 'packages']):
             project_choices.append((path, directory))
     commons = select_commons(project_choices)
-    print(commons)
+    webroles = select_webroles(project_choices, commons)
+    workerroles = select_workerroles(project_choices, commons + webroles)
 
 
-def select_commons(project_choices):
-    print("\nSelect which project to be packaged with all roles (seperated by spaces)")
-    for i, project in enumerate(project_choices):
-        print(i, "-", project[1])
+def get_user_choice(project_choices):
     while True:
         try:
             choice_indexes = [int(n) for n in input("Selection: ").split()]
             return [project_choices[index] for index in choice_indexes]
         except (ValueError, IndexError):
             print("Invalid selection")
+
+
+def select_workerroles(project_choices, ignore):
+    print("\nSelect which project are worker roles (seperated by spaces)")
+    for i, choice in enumerate(project_choices):
+        if choice not in ignore:
+            print(i, "-", choice[1])
+    return get_user_choice(project_choices)
+
+
+def select_webroles(project_choices, ignore):
+    print("\nSelect which project are web roles (seperated by spaces)")
+    for i, choice in enumerate(project_choices):
+        if choice not in ignore:
+            print(i, "-", choice[1])
+    return get_user_choice(project_choices)
+
+def select_commons(project_choices):
+    print("\nSelect which project to be packaged with all roles (seperated by spaces)")
+    for i, project in enumerate(project_choices):
+        print(i, "-", project[1])
+    return get_user_choice(project_choices)
 
     
 
