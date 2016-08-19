34de39e6c40c9e1a6f3c142f07bde37ac0fe7583
Norton Pengra <npengra317@gmail.com>
2016-08-18 20:10:35 -0700

    Select commons created

--- pyscripts/pre_script.py
+++ pyscripts/pre_script.py
@@ -11,12 +11,21 @@ def main(solution_path):
         if os.path.isdir(path) and (directory.lower() not in ['backup', '.vs', 'packages']):
             project_choices.append((path, directory))
     commons = select_commons(project_choices)
+    print(commons)
 
 
 def select_commons(project_choices):
     print("\nSelect which project to be packaged with all roles (seperated by spaces)")
     for i, project in enumerate(project_choices):
         print(i, "-", project[1])
+    while True:
+        try:
+            choice_indexes = [int(n) for n in input("Selection: ").split()]
+            return [project_choices[index] for index in choice_indexes]
+        except (ValueError, IndexError):
+            print("Invalid selection")
+
+    
 
 if __name__ == "__main__":
     main(sys.argv[1])
