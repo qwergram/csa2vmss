5614c9d4630ef3a9baee9590d67d86bc4200f945
Norton Pengra <npengra317@gmail.com>
2016-08-23 17:05:09 -0700

    read template functionality

--- pyscripts/rebuild_arm.py
+++ pyscripts/rebuild_arm.py
@@ -4,6 +4,17 @@ import io
 import json
 import shutil
 
+CURRENT_PATH = os.getcwd()
+
+def read_current_vm_template():
+    with io.open(os.path.join(CURRENT_PATH, "__save", "vmss_template.json")) as context:
+        return json.loads(context.read())
+
+def read_vmss_template():
+    with io.open(os.path.join(CURRENT_PATH, "templates", "vmss.json")) as context:
+        return json.loads(context.read())
+
+
 def main():
     pass
 
