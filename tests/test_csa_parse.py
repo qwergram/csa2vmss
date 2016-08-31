5019a8a3cc58fb817edb25c1935bc8e65ae607e8
Norton Pengra <npengra317@gmail.com>
2016-08-31 09:53:28 -0700

    basic csa_init test

---
+++ tests/test_csa_parse.py
@@ -0,0 +1,12 @@
+
+
+def test_csa_init():
+    from pyscripts.csa_parse import VSCloudService
+    from pyscripts.util import POST_TEST_ENV
+    import os
+    for language in os.listdir(POST_TEST_ENV):
+        for test_unit in os.path.join(POST_TEST_ENV, language):
+            x = VSCloudService(project_path=os.path.join(POST_TEST_ENV, language, test_unit))
+            assert x.guid_dir
+            assert x.solution_data == {}
+            assert x.project_path == os.path.join(POST_TEST_ENV, language, test_unit)
