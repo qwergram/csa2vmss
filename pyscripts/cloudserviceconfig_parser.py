92901268d182eaceffe3f987b87d6318e5531a38
Norton Pengra <npengra317@gmail.com>
2016-08-31 15:12:02 -0700

    new parser for config files

---
+++ pyscripts/cloudserviceconfig_parser.py
@@ -0,0 +1,24 @@
+try:
+    from pyscripts import util
+except ImportError:
+    import util
+import io
+
+class CSConfigParser(object):
+
+    def __init__(self, solution_data):
+        if not isinstance(solution_data, solution_parser.SolutionParser):
+            raise TypeError(type(solution_data))
+        self.location = solution_data.data['cscfg']
+        self.xml = None
+        self.data = None
+
+    def parse(self):
+        self.get_content()
+        self.parse_content()
+
+    def get_content(self):
+        self.xml = util.load_xml(self.location)
+
+    def parse_content(self):
+        pass
