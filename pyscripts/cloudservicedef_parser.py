eb047692324a43911bf74802030c0c7ba2c0d147
Norton Pengra <npengra317@gmail.com>
2016-08-31 14:33:43 -0700

    Create cloudservicedef_parser

---
+++ pyscripts/cloudservicedef_parser.py
@@ -0,0 +1,5 @@
+try:
+    from pyscripts import util
+except ImportError:
+    import util
+import io
--- pyscripts/solution_parser.py
+++ pyscripts/solution_parser.py
@@ -3,7 +3,6 @@ try:
 except ImportError:
     import util
 import io
-import os
 
 class SolutionParser(object):
 
