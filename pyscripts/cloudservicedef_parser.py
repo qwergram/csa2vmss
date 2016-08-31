91f5c36f9475fe3162b2397da93c6913e41eecf6
Norton Pengra <npengra317@gmail.com>
2016-08-31 15:01:34 -0700

    mold parse_content to work in new class

--- pyscripts/cloudservicedef_parser.py
+++ pyscripts/cloudservicedef_parser.py
@@ -18,6 +18,7 @@ class CSDefinitionParser(object):
             raise TypeError(type(solution_data))
         self.location = solution_data.data['csdef']
         self.xml = None
+        self.data
 
 
     def parse(self):
@@ -29,29 +30,32 @@ class CSDefinitionParser(object):
 
 
     def parse_content(self):
+        xml_data = {}
         for role in self.xml.getchildren():
-            role_settings = {}
+            attributes = {key: value for key, value in role.items()}
+            projectname = attributes['name']
+            del attributes['name']
+            xml_data[projectname] = {key: value for key, value in role.items()}
             for setting in role.getchildren():
-                if clean_xml_tag(setting.tag) == "endpoints":
-                    role_settings[clean_xml_tag(setting.tag)] = {}
+                if clean_xml_tag(setting.tag) == 'endpoints':
+                    xml_data[projectname]['endpoints'] = {}
                     for subset in setting.getchildren():
                         subset_items = {key: value for key, value in subset.items()}
-                        role_settings[clean_xml_tag(setting.tag)][subset_items['name']] = {"port": subset_items['port'], "protocol": subset_items["protocol"]}
+                        xml_data[projectname]['endpoints'][subset_items['name']] = {
+                            "port": subset_items['port'], 
+                            "protocol": subset_items["protocol"]
+                        }
                 elif clean_xml_tag(setting.tag) == "configurationsettings":
-                    role_settings[clean_xml_tag(setting.tag)] = {}
+                    xml_data[projectname]["configurationsettings"] = {}
                     for subset in setting.getchildren():
                         for key, value in subset.items():
-                            role_settings[clean_xml_tag(setting.tag)][value] = None
+                            xml_data[projectname]["configurationsettings"][key] = value
                 elif clean_xml_tag(setting.tag) == "sites":
-                    role_settings[clean_xml_tag(setting.tag)] = {"bindings": []}
+                    xml_data[projectname]["sites"] = {"bindings": []}
                     for site in setting.getchildren():
                         for bindings in site.getchildren():
                             for binding in bindings.getchildren():
                                 binding = {key: value for key, value in binding.items()}
                                 if binding.get('endpointName'):
-                                    role_settings[clean_xml_tag(setting.tag)]['bindings'].append(binding['endpointName'])
-                else:
-                    role_settings[clean_xml_tag(setting.tag)] = []
-                    for subset in setting.getchildren():
-                        for key, value in subset.items():
-                            role_settings[clean_xml_tag(setting.tag)].append(value)
+                                    xml_data[projectname]["sites"]['bindings'].append(binding['endpointName'])
+        self.data = xml_data
