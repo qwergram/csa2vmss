63dd83f49130fa0e85114c509344ae1cfbdd4d1e
Norton Pengra <npengra317@gmail.com>
2016-08-31 14:25:16 -0700

    basic parser in place

--- pyscripts/pre_script.py
+++ pyscripts/pre_script.py
@@ -4,6 +4,19 @@ import sys
 import os
 try:
     from pyscripts import util
+    from pyscripts import solution_parser
 except ImportError:
     import util
+    import solution_parser
 
+
+def main(location):
+    pass
+
+
+if __name__ == "__main__":
+    params = util.parse_input()
+    location = util.parse_input_args()
+    if location:
+        main(location)
+    
