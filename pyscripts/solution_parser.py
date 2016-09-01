8bf7794bb83008fed92f4c4f801b8d954d961548
Norton Pengra <npengra317@gmail.com>
2016-09-01 14:03:23 -0700

    set default project references to empty list

--- pyscripts/solution_parser.py
+++ pyscripts/solution_parser.py
@@ -96,4 +96,4 @@ class SolutionParser(object):
             try:
                 self.data['projects'][i]['references'] = proj.data[project['name']]
             except KeyError:
-                continue
+                self.data['projects'][i]['references'] = []
