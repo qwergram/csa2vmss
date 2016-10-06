b33d45a7eb22343a3ae7de10ac6ca768a164b6ef
Norton Pengra <v-nopeng@microsoft.com>
2016-10-06 17:24:09 +0000

    Update proj_parser.py

--- pyscripts/proj_parser.py
+++ pyscripts/proj_parser.py
@@ -7,31 +7,36 @@ class DumbXMLParser(object):
         self.load(location)
     
     def load(self, location):
+        # read xml file
         with io.open(location) as context:
             self.xml = context.readlines()
 
     def tag_search(self, tag):
+        # return line where tag is found
         results = []
         for i, line in enumerate(self.xml):
-            if "<{}".format(tag.lower()) in line.strip().lower():
+            if "<{}".format(tag.lower()) in line.strip().lower(): # just find "<tag" and append to results
                 results.append((i, line))
         return results
 
 
 class ProjParser(object):
 
-    def __init__(self, solution_data):
+    def __init__(self, solution_data): 
+        # pass in SolutionParser object
         if not isinstance(solution_data, solution_parser.SolutionParser):
             raise TypeError(type(solution_data))
-        self.location = solution_data.data['projects']
+        self.location = solution_data.data['projects'] 
         self.xml = None
         self.data = {}
 
     def parse(self):
+        # Parse each project location
         for i, project in enumerate(self.location):
             self.parse_one(i, project['name'])
 
     def parse_one(self, i, projectname):
+        # Parse the XML and parse 
         self.get_content(i)
         self.parse_content(projectname)
 
